ae7c8f729c0fd253b660171ef4c1d84ae4071a95
Seydi Abubakre <aboubakreseydi@gmail.com>
2022-10-04 16:20:07 +0000

    first commit

---
+++ upload-photo/src/main/java/com/uploadphoto/UploadPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.uploadphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadPhotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadPhotoApplication.class, args);
+    }
+
+}
---
+++ upload-photo/src/test/java/com/uploadphoto/UploadPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploadphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadPhotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
